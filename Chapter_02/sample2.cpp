eae950a33a3ca2e1758b7c25129305ef142aceda
Vinícius Godoy de Mendonça <vinigodoy@gmail.com>
2020-08-13 12:41:46 -0300

    Update sample2.cpp
    Fix open method for the default camera.

--- Chapter_02/sample2.cpp
+++ Chapter_02/sample2.cpp
@@ -40,7 +40,7 @@ int main( int argc, const char** argv )
 	if(videoFile != "")
 		cap.open(videoFile);
 	else
-		cap.open("Recording3.webm");
+		cap.open(0);
     if(!cap.isOpened())  // check if we succeeded
         return -1;
 
